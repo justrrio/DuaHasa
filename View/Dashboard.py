55cb09591defee316248cf96c1e88ca35be108a4
Xenodrom31 <revo.rahmat@student.upj.ac.id>
2024-06-20 10:02:47 +0700

    refactor: Update Dashboard class inheritance to QMainWindow

--- View/Dashboard.py
+++ View/Dashboard.py
@@ -796,6 +796,7 @@ class dashboard(QMainWindow):
         "}")
                 self.horizontalLayout_4.addWidget(self.Stars_count)
 
+
                 self.horizontalLayout_6.addLayout(self.horizontalLayout_4)
                 
                 #=============================================================
